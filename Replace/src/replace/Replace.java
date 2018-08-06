87d1d37b08568f8bceb835e173331fa01834f781
Aluno <Aluno@Digital.net>
2018-08-06 20:19:39 -0300

    Adicionada classe ReplaceArquivo

--- Replace/src/replace/Replace.java
+++ Replace/src/replace/Replace.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package replace;
 
 import java.io.IOException;
@@ -7,56 +11,102 @@ import java.nio.file.*;
 import java.util.List;
 
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Replace {
 
     public static void main(String[] args) throws IOException {
+        // TODO code application logic here
 
         Scanner in = new Scanner(System.in);
-        
-        System.err.print("Entre com o local do arquivo");
-        String local = in.next();
-        System.err.print("Entre com o nome do arquivo *e sua extensão: ");
-        String arquivo = in.next();
-        
-        Path path = Paths.get(local, arquivo);
-        Charset charset = Charset.forName("ISO-8859-1");
-                
-        List<String> linhas = Files.readAllLines(path,charset);
-        
-        StringBuilder linha = new StringBuilder();
-        
-        int a = 0;
+
+        boolean continuar = true;
+        List<String> linhas = null;
             
+        while (continuar) {
+            System.err.print("Entre com o local do arquivo");
+            String local = in.next();
+            System.err.print("Entre com o nome do arquivo *e sua extensão: ");
+            String arquivo = in.next();
+
+            Path path = Paths.get(local, arquivo);
+            Charset charset = Charset.forName("ISO-8859-1");
+
+            try {
+                linhas = Files.readAllLines(path, charset);
+                continuar = false;
+            } catch (IOException ex) {
+                System.out.println("Arquivo não encontrado");
+                continue;
+            }
+        }
+        StringBuilder linha = new StringBuilder();
+
+        ArrayList<ReplaceArquivo> repl = new ArrayList<ReplaceArquivo>();
+        repl.add(new ReplaceArquivo("-1", "00"));
+        repl.add(new ReplaceArquivo("AS0M", ""));
+
+        int a = 0;
+
         System.err.println("INICIANDO..");
-        for(String li : linhas){
-            a++;            
-            linha.append(li.replace("-1","00"));
+        for (String li : linhas) {
+            a++;
+
+            for (ReplaceArquivo ra : repl) {
+                li = li.replace(ra.getAntigo(), ra.getNovo());
+            }
+
+            linha.append(li);
             linha.append(System.getProperty("line.separator"));
         }
-        
-        
-      
+
         //System.out.println(linha);
-       System.err.println(a+" LINHAS LIDAS\n\n");
-       
-       System.err.print("Local para salvar: ");
-       String caminhoSave = in.next();
-       
-       System.err.print("Nome do arquivo *sem extensão: ");
-       caminhoSave += in.next()+".txt";
-       
-       System.err.println("O arquivo será salvo em: "+caminhoSave+"\n\n");
-       
-       FileWriter fw = new FileWriter(caminhoSave);
-       
-       System.err.println("INICIANDO GRAVAÇÃO");  
-       
-       fw.write(linha.toString());
-       
-       System.err.println("ARQUIVO SALVO!");
+        System.err.println(a + " LINHAS LIDAS\n\n");
+
+        System.err.print("Diretorio para salvar: ");
+        String caminhoSave = in.next();
+
+        System.err.print("Nome do arquivo *sem extensão: ");
+        caminhoSave += in.next() + ".txt";
+
+        System.err.println("O arquivo será salvo em: " + caminhoSave + "\n\n");
+
+        FileWriter fw = new FileWriter(caminhoSave);
+
+        System.err.println("INICIANDO GRAVAÇÃO");
+
+        fw.write(linha.toString());
+
+        System.err.println("ARQUIVO SALVO!");
     }
-    
+
 }
-       
+
+class ReplaceArquivo {
+
+    private String antigo;
+    private String novo;
+
+    public ReplaceArquivo(String antigo, String novo) {
+        this.antigo = antigo;
+        this.novo = novo;
+    }
+
+    public String getAntigo() {
+        return antigo;
+    }
+
+    public void setAntigo(String antigo) {
+        this.antigo = antigo;
+    }
+
+    public String getNovo() {
+        return novo;
+    }
+
+    public void setNovo(String novo) {
+        this.novo = novo;
+    }
+
+}
